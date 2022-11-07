2be7b5b538d56f3c1b882b505941dd8657ceb0b5
HectorGS7 <hector.gasca1810@gmail.com>
2022-11-06 20:55:14 -0600

    Implementación de la barra de búsqueda

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,44 +34,40 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="navbar">
-      <a href="/" className="navbar__logo">
-        <img src="/images/logo.svg" alt="Logo Invit-Arte" />
-      </a>
-      <button
-        className="button__menu"
-        onClick={() => setMenuIsVisible(!menuIsVisible)}
-      >
-        <i
-          className={`fa-solid ${menuIsVisible ? "fa-x" : "fa-bars-staggered"}`}
-        ></i>
-      </button>
-      <div className={`navbar__menu ${menuIsVisible && "showMenu"}`}>
-        <ul className="navbar__menu-container">
-          <li className="navbar__menu-search">
-            <input type="search" placeholder="Buscar en Invit-Arte" />
-            <button>
-              <i className="fa-solid fa-magnifying-glass"></i>
-            </button>
-          </li>
-          <li>
-            {user ? (
-              <button onClick={signOut}>
-                Cerrar Sesión{" "}
-                <span className="button__signOut-name">({user.name})</span>
-              </button>
-            ) : (
-              <a href="/login/">acceder</a>
-            )}
-          </li>
-          {links.map(({ text, url }) => (
-            <li key={text}>
-              <a href={url}>{text}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
+
+        <nav className="navbar">
+          <a href="/" className="navbar__logo">
+            <img src="/images/logo.svg" alt="Logo Invit-Arte" />
+          </a>
+          <button
+            className="button__menu"
+            onClick={() => setMenuIsVisible(!menuIsVisible)}
+          >
+            <i
+              className={`fa-solid ${menuIsVisible ? "fa-x" : "fa-bars-staggered"}`}
+            ></i>
+          </button>
+          <div className={`navbar__menu ${menuIsVisible && "showMenu"}`}>
+            <ul className="navbar__menu-container">
+              
+              <li>
+                {user ? (
+                  <button onClick={signOut}>
+                    Cerrar Sesión{" "}
+                    <span className="button__signOut-name">({user.name})</span>
+                  </button>
+                ) : (
+                  <a href="/login/">acceder</a>
+                )}
+              </li>
+              {links.map(({ text, url }) => (
+                <li key={text}>
+                  <a href={url}>{text}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </nav>
   );
 };
 
