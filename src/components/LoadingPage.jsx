b325c1cd2aa0617214ece6880fc0e15fc7194dec
baezdev <baezdev@gmail.com>
2022-11-17 00:05:36 -0600

    pagina de loading

---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const LoadingPage = () => {
+  return (
+    <section className="page__complete center__section">
+      <div className="center__section-column" style={{ width: "100%" }}>
+        <span class="loader"></span>
+      </div>
+    </section>
+  );
+};
