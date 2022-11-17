2e4335ad69e3fc1674d1f2267705043bc1ebf9c0
baezdev <baezdev@gmail.com>
2022-11-16 20:54:06 -0600

    servidor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
-/* import vercel from "@astrojs/vercel/serverless"; */
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  /* output: "server",
-  adapter: vercel() */
+  output: "server",
+  adapter: vercel(),
 });
